e6c4032725b5f947a5b0e0b0b742d3b6a7f37ed7
Pablo Rodriguez rodriguez <pablorodriguezrodriguez@MacBook-Air-de-Pablo.local>
2024-06-17 09:56:42 +0200

    deteccion de manos hecha

--- hand_detection.py
+++ hand_detection.py
@@ -1,2 +1,17 @@
 import cv2
 import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mp_drawing = mp.solutions.drawing_utils
+
+def detect_hands(frame):
+    frame_rgb=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    result=hands.process(frame_rgb)
+    return result
+
+
+def draw_hands(frame,result):
+    if result.multi_hand_landmarks:
+        for hand_landmarks in result.multi_hand_landmarks:
+            mp_drawing.drawing_landmarks(frame ,hand_landmarks, mp_hands.HAND_CONNECTIONS)
